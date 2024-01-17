79741cda892a092a756e02b7104f3c33d4abc29f
kyoTran1009 <quangdieuisme@gmail.com>
2024-01-17 21:32:14 +0700

    add and fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/include/init-db.php");
 require_once(__DIR__ . "/classes/user.php");
 
 // Go to homepage
-require_once(__DIR__ . "/home/index.php");
+require_once(__DIR__ . "/src/home/index.php");
 
 
 ?>
--- utils/config.php
+++ utils/config.php
@@ -11,11 +11,29 @@
   ----------------------------------------------------------------------------------
 
 */
+
+// define connection
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'db_accounts');
 define('DB_USER', 'ad_db_ct07');
 define('DB_PASSWORD', 'admin');
 define('DB_PORT', '3306');
 define('DB_SOCKET', '/Applications/AMPPS/apps/mysql/var/mysql.sock');
+
+// define Google API configuration
+define('client_id', '561685310040-3508vsi0vm2td439n8f2uiv9gattqp9r.apps.googleusercontent.com');
+define('client_secret', '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');
+define('redirect_uri', 'http://localhost:3000/Projects/src/home/sign-in/Oauth/oauth-google.php');
+
+//  set Client Configs in Google API
+require(__DIR__ . "/../vendor/autoload.php");
+$client = new Google\Client();
+$client->setClientId(client_id);
+$client->setClientSecret(client_secret);
+
+$client->setRedirectUri(redirect_uri);
+$client->addScope("email");
+$client->addScope("profile");
+
 
 ?>
