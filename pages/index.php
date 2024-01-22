c7374a97961aec6b7afaba4a29ae9b6e7cdb9295
KyoTran <quangdieuisme@gmail.com>
2024-01-23 00:21:49 +0700

    fix something

--- pages/index.php
+++ pages/index.php
@@ -11,14 +11,12 @@
     $basePath = rtrim(str_replace($_SERVER['DOCUMENT_ROOT'], '', __DIR__), '/');;
     ?>
 
-    <div>
-        <a href= "<?php echo $basePath; ?>/sign-in/log-in/index.html"> Đăng nhập hệ thống  </a> &nbsp;
-        <a href= "<?php echo $basePath; ?>/sign-in/Oauth/oauth-google.php"> Đăng nhập bằng tài khoản google  </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/sign-up/index.html"> Đăng ký tài khoản  </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/homepage/homepage.php"> Trang chủ </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/sign-in/log-out/index.php?type=normal" > Đăng xuất </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/sign-in/log-out/index.php?type=google"> Đăng xuất Google </a> &nbsp;
-
-    </div>    
+<div>
+        <a href="<?php echo $basePath; ?>/../auth/login_form.html"> Đăng nhập hệ thống </a> &nbsp;
+        <a href= "<?php echo $basePath; ?>/../auth/google_login.php"> Đăng nhập bằng tài khoản google  </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/signup_form.html"> Đăng ký tài khoản </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/logout.php?type=normal"> Đăng xuất </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/logout.php?type=google"> Đăng xuất Google </a> &nbsp;
+    </div>
 </body>
 </html>
