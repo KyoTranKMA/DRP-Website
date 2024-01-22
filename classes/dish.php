c1725d582e755674c6193b45f97a5439612e5db9
KyoTran <quangdieuisme@gmail.com>
2024-01-23 00:21:28 +0700

    add db_extend & dish

--- classes/dish.php
+++ classes/dish.php
@@ -6,25 +6,39 @@ class Dish
     private $name;
     private $description;
     private $ingredient;
-    private $nutrition;
+    private $nutrition; //Lượng dinh dưỡng của món ăn
     private $author;
     private $imagefile;
 
 
 
-    public function __construct($title, $description, $author, $imagefile)
+    public function __construct($name, $description, $ingredient, $nutrition ,$author, $imagefile)
     {
-        $this->title = $title;
+        $this->name = $name;
         $this->description = $description;
+        $this->ingredient = $ingredient;
+        $this->nutrition = $nutrition;
         $this->author = $author;
         $this->imagefile = $imagefile;
     }
     private function validate()
     {
         if (!file_exists($this->imagefile)) {
-            return $this->title != '' &&
+            // Image file does not exist
+            return $this->name != '' &&
                 $this->description != '' &&
+                $this->ingredient != '' &&
+                $this->nutrition != '' &&
                 $this->author != '';
+        }
+        else{
+            // Image file exists
+            return $this->name != '' &&
+                $this->description != '' &&
+                $this->ingredient != '' &&
+                $this->nutrition != '' &&
+                $this->author != '' &&
+                $this->imagefile != '';
         }
     }
 
@@ -35,13 +49,15 @@ class Dish
     public function add($connection){
         if($this->validate()){
             try{
-                $sql = "insert into books (title, description, author, imagefile) values (:title, :description, :author, :imagefile)";
+                $sql = "insert into dishs (name, description, ingredient, nutrition , author, imagefile) values (:name, :description, :ingredient, :nutrition, :author, :imagefile)";
                 // Prepare the statement
                 $stmt = $connection->prepare($sql);
 
                 // Bind parameters
-                $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+                $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
                 $stmt->bindValue(':description', $this->description, PDO::PARAM_STR);
+                $stmt->bindValue(':ingredient', $this->ingredient, PDO::PARAM_STR);
+                $stmt->bindValue(':nutrition', $this->nutrition, PDO::PARAM_STR);
                 $stmt->bindValue(':author', $this->author, PDO::PARAM_STR);
                 $stmt->bindValue(':imagefile', $this->imagefile, PDO::PARAM_STR);
                 return $stmt->execute();
@@ -55,10 +71,10 @@ class Dish
     }
     public static function getAll($connection){
         try{
-            $sql = "select * from books order by title asc";
+            $sql = "select * from dishs order by title asc";
             // Prepare the statement
             $stmt = $connection->prepare($sql);
-            $stmt->setFetchMode(PDO::FETCH_CLASS,"Book");
+            $stmt->setFetchMode(PDO::FETCH_CLASS,"Dish");
             if( $stmt->execute() ){
                 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 return ($books);
@@ -73,14 +89,14 @@ class Dish
 
     public function getByID($connection, $id){
         try{
-            $sql = "select * from books where id=:id";
+            $sql = "select * from dishs where id=:id";
             // Prepare the statement
             $stmt = $connection->prepare($sql);
             $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
-            $stmt->setFetchMode(PDO::FETCH_CLASS,"Book");
+            $stmt->setFetchMode(PDO::FETCH_CLASS,"Dish");
             if( $stmt->execute() ){
-                $book = $stmt->fetch();
-                return $book;
+                $Dish = $stmt->fetch();
+                return $Dish;
             }
         }
         catch(PDOException $e){
@@ -93,7 +109,7 @@ class Dish
     public function update($connection){
         try{
             // Add them title author ... index
-            $sql = "UPDATE `books` SET `id`='[value-1]',`title`='[value-2]',`description`='[value-3]',`author`='[value-4]',`imagefile`='[value-5]' WHERE id:=id";
+            $sql = "INSERT INTO `dishs`(`idDish`, `nameDish`, `description`, `ingredient`, `nutrition`, `author`, `imagefile`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')";
             $stmt = $connection->prepare($sql);
         }
         catch(PDOException $e){
