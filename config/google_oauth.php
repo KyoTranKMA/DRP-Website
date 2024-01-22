bb8cfcdfe34b5d9a3a3677913e6dba407be0421d
KyoTran <quangdieuisme@gmail.com>
2024-01-23 00:21:49 +0700

    fix something

--- config/google_oauth.php
+++ config/google_oauth.php
@@ -3,7 +3,7 @@
 // define Google API configuration
 define('client_id', '561685310040-3508vsi0vm2td439n8f2uiv9gattqp9r.apps.googleusercontent.com');
 define('client_secret', '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');
-define('redirect_uri', 'http://localhost:3000/Projects/auth/google_login.php');
+define('redirect_uri', 'http://localhost:3000/auth/google_login.php');
 
 //  set Client Configs in Google API
 require(__DIR__ . "/../vendor/autoload.php");
--- pages/homepage.php
+++ pages/homepage.php
@@ -20,11 +20,11 @@
     ?>
 
     <div>
-        <a href="<?php echo $basePath; ?>/../sign-in/log-in/index.html"> Đăng nhập hệ thống </a> &nbsp;
-        <a href= "<?php echo $basePath; ?>/../sign-in/Oauth/oauth-google.php"> Đăng nhập bằng tài khoản google  </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/../sign-up/index.html"> Đăng ký tài khoản </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/../sign-in/log-out/index.php?type=normal"> Đăng xuất </a> &nbsp;
-        <a href="<?php echo $basePath; ?>/../sign-in/log-out/index.php?type=google"> Đăng xuất Google </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/login_form.html"> Đăng nhập hệ thống </a> &nbsp;
+        <a href= "<?php echo $basePath; ?>/../auth/google_login.php"> Đăng nhập bằng tài khoản google  </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/signup_form.html"> Đăng ký tài khoản </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/logout.php?type=normal"> Đăng xuất </a> &nbsp;
+        <a href="<?php echo $basePath; ?>/../auth/logout.php?type=google"> Đăng xuất Google </a> &nbsp;
     </div>
     <?php
         require_once(__DIR__ . "/../includes/init.php");
