9a404664260077828d949e24c4f8148635e8b6c4
user.email <quangdieuisme@gmail.com>
2024-02-10 23:44:09 +0700

    fix user

--- server/classes/user.php
+++ server/classes/user.php
@@ -1,16 +1,50 @@
 <?php
 class User
 {
-    // Delcare Propertys of DB
-    public $id;
+    /*
+        This class is manager 
+            authenticate(): Login
+            addUser(): Register
+    */
     public $username;
-    public $password;
+    public $password; 
+    public $firstname;
+    public $lastname;
+    public $dateofbirth;
+    public $email;
+    public $country;
+    public $gender;
+    public $connection;
 
-    private function validate(){
-        return $this->username !=  '' && $this->password != ''; 
-    } 
+    private function checkEmail($email){
+        $sql = "select from users where email=:email LIMIT 1";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+        $stmt->execute();
+        $user = $stmt->fetch();
+        if ($user)
+        {
+            return true;
+        } 
+        return false;
+    }
 
-    // Authentication User
+    private function checkUserName($username){
+        $sql = "select from users where username=:username LIMIT 1";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+        $stmt->execute();
+        $user = $stmt->fetch();
+        if ($user) 
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Authenticate user (login)
     public static function authenticate($connection, $username, $password)
     {
         $sql = "select * from users where username=:username";
@@ -33,23 +67,40 @@ class User
         // Return false if the user is not found 
         return false;
     }
-    
-    // Add new Users
-    public function addUser($connection){
-        if($this->validate()){
-            // Insert data into the 'users' table 
-            $sql = "insert into users (username, password) values (:username, :password)";
-            // Prepare the statement
-            $stmt = $connection->prepare($sql);
 
-            // Bind parameters
-            $stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
-            $hashPassword = password_hash($this->password, PASSWORD_DEFAULT);
-            $stmt->bindValue(':password', $hashPassword, PDO::PARAM_STR);
-            return $stmt->execute();
+    public function addUser($connection,$username, $password, $firstname , $lastname, $dateofbirth, $email, $country, $gender){
+        $this->connection = $connection;
+
+        // check username and email has been
+        if ($username == '' || $password == '' || $firstname == '' || $lastname == '' || $dateofbirth == '' || $email == '' || $country == '' || $gender == ''){
+            $alert = 'Fields must be not empty!';
+            return $alert;
+        } else {
+            if ($this->checkEmail($email)){
+                $alert = 'Email Already Existed';
+                return $alert;
+            };
+            if ($this->checkUserName($username)){
+                $alert = 'Username Already Existed';
+                return $alert;
+            }
         }
-        // If Username and PassWord not valid
-        return false;
-    }
 
+        $sql = "insert into users (username, password, firstname, lastname, dateofbirth, email, country, gender) values (:username, :password, :firstname, :lastname, :dateofbirth, :email, :country, :gender)";
+        // Prepare the statement
+        $stmt = $connection->prepare($sql);
+        // Bind parameters
+        $stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
+        $hashPassword = password_hash($this->password, PASSWORD_DEFAULT);
+        $stmt->bindValue(':password', $hashPassword, PDO::PARAM_STR);
+        $stmt->bindValue(':firstname', $this->firstname, PDO::PARAM_STR);
+        $stmt->bindValue(':lastname', $this->lastname, PDO::PARAM_STR);
+        $stmt->bindValue(':dateofbirth', $this->dateofbirth, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $this->email, 
+        PDO::PARAM_STR);
+        $stmt->bindValue(':country', $this->country, PDO::PARAM_STR);
+        $stmt->bindValue(':gender', $this->gender, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+    
 }
