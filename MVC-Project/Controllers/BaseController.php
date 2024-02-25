d75f7ad995b483026855df1335421ae45b1869da
user.email <quangdieuisme@gmail.com>
2024-02-26 01:00:37 +0700

    add controllers

--- MVC-Project/Controllers/BaseController.php
+++ MVC-Project/Controllers/BaseController.php
@@ -6,13 +6,30 @@ class BaseController
         pathname: foldername.filename
     */
     const VIEW_FOLDER_NAME = 'Views';
+    const MODEl_FOLDER_NAME = 'Models';    
+    protected $dbController;
 
-    protected function loadView($viewPath)
+    // Constructer Connection to DB 
+    public function __construct(DatabaseController $db)
     {
-        $viewPath = self::VIEW_FOLDER_NAME . '/'.  str_replace('.', '/', $viewPath) . '.php';
-        echo $viewPath;
-        require("./$viewPath"); 
+        $this->dbController = $db;
+    }
+
+    // Create Method for get classes in Views
+    protected function loadView($viewPath, array $data = [])
+    {
+        foreach($data as $key => $value)
+        {
+            $$key = $value;
+        }
+        
+        require('./' . self::VIEW_FOLDER_NAME . '/'.  str_replace('.', '/', $viewPath) . '.php'); 
     } 
+    // Create Method for get classes in Models
+    protected function loadModel($modelPath)
+    {
+        require('./' . self::MODEl_FOLDER_NAME . '/'.  str_replace('.', '/', $modelPath) . '.php');
+    }
 
 }
 
--- MVC-Project/Controllers/HomeController.php
+++ MVC-Project/Controllers/HomeController.php
@@ -1,1 +1,22 @@
-<h1>Homne Controller</h1>
+<?php
+
+class HomeController extends BaseController
+{
+    public function index()
+    {
+        return $this->loadView('frontend.pages.index');
+    }
+    public function homePage()
+    {
+        return $this->loadView('frontend.pages.homepage');
+    }
+
+
+}
+
+
+
+
+
+
+?>
