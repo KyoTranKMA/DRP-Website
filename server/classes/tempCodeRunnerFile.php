6e15469d8ecdec08e04cc28775741debab07d1ae
JohnHooin <151376043+JohnHooin@users.noreply.github.com>
2024-02-26 14:08:25 +0700

    Delete server directory

--- server/classes/tempCodeRunnerFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $fraction = 0;
-  // if (isset($matches[2]) && !empty($matches[2])) {
-  //     list($numerator, $denominator) = explode('/', $matches[2]);
-  //     $fraction =  (float)($numerator) / (float)($denominator);
-  // }
-  // $total = intval($matches[1]) + $fraction;
-  // return number_format($total, 5); // round the decimal to 5
--- server/utils/php_scripts/add_ingredient.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/server/index.php");
-    Add_Ingre_Handler::handlePostRequest($conn);
-  }
-?>
