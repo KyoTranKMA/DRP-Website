10924530eba67f2a5b995a6bea34598b53c4d305
user.email <quangdieuisme@gmail.com>
2024-02-27 18:11:13 +0700

    update

--- Web/App/index.php
+++ Web/App/index.php
@@ -4,10 +4,7 @@
     require_once(__DIR__ . '/Core/Database.php');
     require_once(__DIR__ . '/Models/BaseModel.php');
    
-    $db = new DataBase;
-    $db->getConnection();
-    $model = new BaseModel();
-    $model->getConnection();
+   
 
     
 
