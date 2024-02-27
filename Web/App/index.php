be8946169ea929cf4d73a759d052de3451244398
user.email <quangdieuisme@gmail.com>
2024-02-28 01:23:04 +0700

    update index

--- Web/App/index.php
+++ Web/App/index.php
@@ -1,10 +1,20 @@
 <?php
 
-    require_once(__DIR__ . '/inc/init.php');
+    require_once(__DIR__ . '/Core/init.php');
     require_once(__DIR__ . '/Core/Database.php');
     require_once(__DIR__ . '/Models/BaseModel.php');
+    require_once(__DIR__ . '/Controllers/BaseController.php');
    
-   
+    // Get Route name and Validate Param
+    $controllerName = ucfirst(strtolower($_REQUEST[('controller')]) . 'Controller');
+    $actionName = $_REQUEST['action'] ?? 'index';
+    require_once(__DIR__ . "/Controllers/$controllerName.php");
+    
+    // Generate Object  
+    $controllerObject = new $controllerName;
+    // Get Method
+    $controllerObject->$actionName();
+    
 
     
 
