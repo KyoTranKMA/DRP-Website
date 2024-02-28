cd18d8483854de49f3529d8bee9b5d311059ea73
user.email <quangdieuisme@gmail.com>
2024-02-28 22:31:48 +0700

    update User Controller

--- Web/App/Controllers/UserController.php
+++ Web/App/Controllers/UserController.php
@@ -1,48 +1,48 @@
 <?php
 class UserController extends BaseController
 {
-    private static $userModel = new UserModel;
+    private $userModel;
     // Get Path Class User Model;
     public function __construct()
     {
         $this->loadModel('UserModel');
+        $this->userModel = new UserModel();
     } 
 
     public function index()
     {
-        // Get Path Class User
-        //$this->userModel = new UserModel;
-        
+        $email = $_POST('email');
+        $password = $_POST('password');
+        $users = $this->userModel->authenticate([$email, $password]);
+        if($users)
+        {
+            return $this->loadView('pages.homepaage', $users);
+        }
     }
 
     public function login(){
-        /*
-            Form đăng nhập của BT đang để là email
-        */
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])){
             $email = $_POST['email'];
             $password = $_POST['password'];
     
-            if (DatabaseController::getInstance()){
-                // Chung thuc user
-                $rs = UserModel::authenticate(DatabaseController::getInstance(), $_POST);
-                if ($rs){
-                    $_SESSION['email'] = $email;
-                    /*header("Location: index.php");
-                    exit();*/
-                } else {
-                    // Báo lỗi login
-                    //echo "Invalid Username or password";
-                }
+            $rs = $this->userModel->authenticate($_POST);
+            if ($rs){
+                $_SESSION['email'] = $email;
+                /*header("Location: index.php");
+                exit();*/
+            } else {
+                // Báo lỗi login
+                echo "Invalid Username or password";
             }
-        }
+            }
     }
 
     public function register(){
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register'])){
-            if (DatabaseController::getInstance()){
-                $rs = $this->userModel->addUser(DatabaseController::getInstance(), $_POST);
-            }
+        $rs = $this->userModel->addUser($_POST);
         }
     }
+
 }
+
+?>
