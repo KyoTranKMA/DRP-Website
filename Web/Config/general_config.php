ab7e66abc182a49a4e7c34248e3464e20ca52054
John Naeder <johnnaeder6537@gmail.com>
2024-03-02 23:44:15 +0700

    Add docker load database dump for mysql

--- Web/Config/general_config.php
+++ Web/Config/general_config.php
@@ -3,5 +3,9 @@
 define('FILE_MAX_SIZE', 2 * 1024 * 1024); //2MB
 define('FILE_TYPE', ['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/bmp', 'image/webp']); 
 
+// Define path 
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
+
+
 // Debug mode on
 define('DEBUG', true);
