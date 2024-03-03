80da919e5af0a73a4fe07a2e83b1bb5ca54fc475
--global <quangdieuisme@gmail.com>
2024-03-03 21:39:59 +0700

    update

---
+++ Web/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'web/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '238e83fa7c383866408abe7ee9e1ef14ef1750ea',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'web/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '238e83fa7c383866408abe7ee9e1ef14ef1750ea',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
