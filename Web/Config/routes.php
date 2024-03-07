139fc27a7af47bb5e27dc5f85be2cf5fd175c5c2
KyoTranKMA <quangdieuisme@gmail.com>
2024-03-07 19:08:18 +0700

    add \

--- Web/Config/routes.php
+++ Web/Config/routes.php
@@ -2,4 +2,7 @@
 const App = '/App';
 
 $router->get(App . '/index.php', 'HomeController@index');
-$router->get(App . '/cong-thuc', 'RecipeController@index');
+$router->get(App . '/index.php/cong-thuc', 'RecipeController@index');
+$router->get(App . '/index.php/thanh-phan', 'IngredientController@index');
+
+$router->get(App . '/index.php/login', 'Auth\\LoginController@show');
