1f9b9151efa38e4905c9e8fbb7abcc9b609dfe7a
KyoTranKMA <quangdieuisme@gmail.com>
2024-03-09 02:10:29 +0700

    update(routes)

--- Web/Config/routes.php
+++ Web/Config/routes.php
@@ -1,25 +1,42 @@
 <?php
-const App = '/App';
 
+// homepage router
+$router->get('/index.php', 'HomeController@homePage');
+$router->get('/homepage', 'HomeController@homePage');
 
-/* Action mặc định (index) -> Mục đích từ index trong Controller gọi đến Views trả về giao diện khi người dùng gọi đến URI bên dưới */
-$router->get(App . '/index.php', 'HomeController@index');
-$router->get(App . '/trang-chu', 'HomeController@homePage');
-$router->get(App . '/cong-thuc', 'RecipeController@index');
-$router->get(App . '/thanh-phan', 'IngredientController@index');
-$router->get(App . '/upload', 'UploadController@index');
-
-$router->get(App . '/user','UserController@index');
-$router->get(App . '/user/login','UserController@loginUI');
-$router->get(App . '/user/registery','UserController@registeryUI');
-$router->get(App . '/user/logout','UserController@logout');
-$router->get(App . '/manager/user', 'Admin@userAccount');
-$router->get(App . '/manager/recipe', 'Admin@recipeManager');
-$router->get(App . '/manager/ingredient', 'Admin@ingredientsManager');
-
-
-/* Action đăng ký */
+// user router
+$router->get('/user','UserController@index');
+$router->get('/user/login','UserController@loginUI');
 $router->post('/user/login','UserController@login');
+$router->get('/user/registery','UserController@registeryUI');
 $router->post('/user/registery','UserController@registery');
+$router->get('/user/logout','UserController@logout');
+// $router->get('/user/profile','UserController@profile');
+// $router->get('/user/edit','UserController@editUI');
+// $router->post('/user/edit','UserController@edit');
 
+// admin router
+$router->get('/manager/user', 'Admin@userAccount');
+$router->get('/manager/recipe', 'Admin@recipeManager');
+$router->get('/manager/ingredient', 'Admin@ingredientsManager');
+$router->get('/manager/recipe/add', 'Admin@addRecipeUI');
 
+// ingredient router
+$router->get('/ingredient','IngredientController@index');
+$router->get('/ingredient/list','IngredientController@listAll');
+$router->get('/ingredient/add','IngredientController@addUI');
+$router->post('/ingredient/add','IngredientController@add');
+$router->get('/ingredient/edit','IngredientController@editUI');
+$router->post('/ingredient/edit','IngredientController@edit');
+$router->get('/ingredient/delete','IngredientController@delete');
+
+// recipe router
+$router->get('/recipe','RecipeController@index');
+$router->get('/recipe/list','RecipeController@list_all');
+$router->get('/recipe/add','RecipeController@addUI');
+$router->post('/recipe/add','RecipeController@add');
+$router->get('/recipe/edit','RecipeController@editUI');
+$router->post('/recipe/edit','RecipeController@edit');
+$router->get('/recipe/delete','RecipeController@delete');
+$router->get('/recipe/show','RecipeController@show');
+$router->get('/recipe/search','RecipeController@search');
