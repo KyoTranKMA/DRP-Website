511d190fbd6d7b84fa1f2b2580e235fa92d6a99f
John Naeder <johnnaeder6537@gmail.com>
2024-03-08 19:27:31 +0700

    Set output error into log file

--- Web/Config/general_config.php
+++ Web/Config/general_config.php
@@ -5,12 +5,16 @@ define('FILE_TYPE', ['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image
 
 // Define path 
 define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
-define('VIEWS_PATH', ROOT_PATH . '/App/Views/');
-define('CONTROLLERS_PATH', ROOT_PATH . '/App/Controllers/');
-define('ERRORS_PATH', ROOT_PATH . '/App/Errors/');
-define('MODELS_PATH', ROOT_PATH . '/App/Models/');
-define('CORE_PATH', ROOT_PATH . '/App/Core/');
-define('UPLOAD_PATH', ROOT_PATH . '/Public/uploads/');
-
+define('VIEWS_PATH', ROOT_PATH . 'App/Views/');
+define('CONTROLLERS_PATH', ROOT_PATH . 'App/Controllers/');
+define('ERRORS_PATH', ROOT_PATH . 'App/Errors/');
+define('MODELS_PATH', ROOT_PATH . 'App/Models/');
+define('CORE_PATH', ROOT_PATH . 'App/Core/');
+define('UPLOAD_PATH', ROOT_PATH . 'Public/uploads/');
+define('DB_RELATED_LOG', ROOT_PATH . 'Journal/database-related.log');
+define('ERROR_LOG', ROOT_PATH . 'Journal/errors.log');
+define('EXCEPTION_LOG', ROOT_PATH . 'Journal/exceptions.log');
+define('DEBUG_LOG', ROOT_PATH . 'Journal/debug.log');
+define('FATAL_ERROR_LOG', ROOT_PATH . 'Journal/fatal-errors.log');
 // Debug mode on
 define('DEBUG', true);
