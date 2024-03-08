53d86112f229baf12d234b5290cb90cb38111155
John Naeder <johnnaeder6537@gmail.com>
2024-03-08 19:25:26 +0700

    Set output error into log file

---
+++ Web/App/Core/Logger.php
@@ -0,0 +1,21 @@
+<?
+namespace App\Core;
+class Logger{
+  private $logFile;
+  public function __construct($logFile){
+    $this->logFile = $logFile;
+  }
+  public static function logError($logFile, $message){
+    if (!file_exists($logFile)) {
+      $file = fopen($logFile, 'w');
+      if ($file === false) {
+          die('Unable to create log file.');
+      }
+      fclose($file);
+    }
+    $dateTime = date('d-m-Y s:i:H');
+    $logMessage = $dateTime . ': ' . $message . PHP_EOL;
+    error_log($logMessage, 3, $logFile);
+  }
+}
+?>
--- Web/App/Core/init.php
+++ Web/App/Core/init.php
@@ -1,28 +1,51 @@
 <?php
-  if (!isset($_SESSION)){
-    session_start();
+use App\Core\Logger;
+
+if (!isset($_SESSION)){
+  session_start();
+}
+require_once($_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/Config/general_config.php");
+
+set_error_handler('handleError');
+set_exception_handler('handleException');
+register_shutdown_function('handleFatalError');
+
+function handleError(int $errno, string $errstr, string $errfile, int $errline) {
+  $errorType = getErrorType($errno);
+  Logger::logError(ERROR_LOG, "$errorType: [$errno] $errstr - $errfile:$errline");
+}
+
+function handleException($exception) {
+  Logger::logError(EXCEPTION_LOG, "Uncaught Exception: " . $exception->getMessage());
+}
+
+function handleFatalError() {
+  $lastError = error_get_last();
+  if ($lastError && $lastError['type'] === E_ERROR) {
+    $errorType = getErrorType($lastError['type']);
+    Logger::logError(FATAL_ERROR_LOG, "$errorType: " . $lastError['message']);
   }
-  require_once($_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php");
-  require_once($_SERVER['DOCUMENT_ROOT'] . "/Config/general_config.php");
+}
 
-//   function exceptionHandler($exception){
-//     if(DEBUG){
-//         echo "<h2>Lỗi: " . $exception->getMessage() . "</h2>";
-//         echo "<p>Noi dung: " . $exception->getMessage() . "</p>";
-//         echo "<p>File: " . $exception->getFile() . "<br> Line: " .  $exception->getLine() . "<p>";
-//     } else {
-//         echo "<h2>Có lỗi xảy ra, vui lòng thử lại sau</h2>";
-//         require_once("Errors/404.php");
-//     }
-//     exit();
-//   }   
-
-
-//   function errorHandler($level, $message, $file, $line){
-//         throw new ErrorException($level, $message, $file, $line);
-//   }
-
-
-//   set_exception_handler('exceptionHandler');
-//   set_error_handler('errorHandler');
+function getErrorType($errno) {
+  $errorTypes = array(
+    E_ERROR             => 'E_ERROR',
+    E_WARNING           => 'E_WARNING',
+    E_PARSE             => 'E_PARSE',
+    E_NOTICE            => 'E_NOTICE',
+    E_CORE_ERROR        => 'E_CORE_ERROR',
+    E_CORE_WARNING      => 'E_CORE_WARNING',
+    E_COMPILE_ERROR     => 'E_COMPILE_ERROR',
+    E_COMPILE_WARNING   => 'E_COMPILE_WARNING',
+    E_USER_ERROR        => 'E_USER_ERROR',
+    E_USER_WARNING      => 'E_USER_WARNING',
+    E_USER_NOTICE       => 'E_USER_NOTICE',
+    E_STRICT            => 'E_STRICT',
+    E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+    E_DEPRECATED        => 'E_DEPRECATED',
+    E_USER_DEPRECATED   => 'E_USER_DEPRECATED'
+  );
+  return $errorTypes[$errno] ?? 'UNKNOWN';
+}
 ?>
