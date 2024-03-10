3e2d5d87e49e490f7accca50a31488e27a124b24
KyoTranKMA <quangdieuisme@gmail.com>
2024-03-11 00:25:13 +0700

    resize of header

--- Web/Public/inc/header.php
+++ Web/Public/inc/header.php
@@ -1,36 +1,57 @@
 <?
+
 use App\Controllers\UserController;
 ?>
 
 <!-- Navbar -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://kit.fontawesome.com/a2af703eed.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="vendors/font-awesome-4.7.0/css/font-awesome.min.css" />
-    <title>PaPals-Enjoy your meals</title>
-    <style>
-        .divider:after,
-        .divider:before {
-            content: "";
-            flex: 1;
-            height: 1px;
-            background: #eee;
-        }
-        .h-custom {
-            height: calc(100% - 73px);
-        }
-        @media (max-width: 450px) {
-            .h-custom {
-                height: 100%;
-            }
-        }
-        </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/a2af703eed.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="vendors/font-awesome-4.7.0/css/font-awesome.min.css" />
+  <title>PaPals-Enjoy your meals</title>
+  <style>
+    .divider:after,
+    .divider:before {
+      content: "";
+      flex: 1;
+      height: 1px;
+      background: #eee;
+    }
+
+    .h-custom {
+      height: calc(100% - 73px);
+    }
+
+    @media (max-width: 450px) {
+      .h-custom {
+        height: 100%;
+      }
+    }
+
+    .navbar {
+      height: 80px;
+    }
+
+    .navbar-brand img {
+      height: 35px
+    }
+
+    .navbar-brand h3 {
+      font-size: 20px
+    }
+
+    .navbar-nav .nav-link {
+      font-size: 15px
+    }
+  </style>
 </head>
+
 <body>
   <!-- <div class="loader_container">
     <span class="loader">Loading....</span>
@@ -39,28 +60,15 @@ use App\Controllers\UserController;
     <!-- Container wrapper -->
     <div class="container-fluid">
       <!-- Toggle button -->
-      <button
-        data-mdb-collapse-init
-        class="navbar-toggler"
-        type="button"
-        data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button data-mdb-collapse-init class="navbar-toggler" type="button" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
       </button>
-  
+
       <!-- Collapsible wrapper -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Navbar brand -->
         <a class="navbar-brand mt-2 mt-lg-0" href="#">
-          <img
-            src="/Public/images/logo.png"
-            height="50"
-            alt="PaPals"
-            loading="lazy"
-          />
+          <img src="/Public/images/logo.png" height="50" alt="PaPals" loading="lazy" />
           <h3>PaPals</h3>
         </a>
         <!-- Left links -->
@@ -81,44 +89,36 @@ use App\Controllers\UserController;
         <!-- Left links -->
       </div>
       <!-- Collapsible wrapper -->
-  
+
       <!-- Right elements -->
-      
-            <?php
-            // Kiểm tra xem đã đăng nhập chưa            
-            if (UserController::isLoggedIn()) {
-                // Nếu đã đăng nhập, hiển thị nút logout và account
-                ?>
-                  <div class="d-flex align-items-center">
-                  <img
-                    src="/Public/images/account.png"
-                    id="user"
-                    class="rounded-circle me-3 account hide"
-                    height="30"
-                    alt="Black and White Portrait of a Man"
-                  />
-                  <div class="d-flex align-items-center">
-                    <a href="/user/logout" class="btn btn-primary btn-lg " 
-                      tabindex="-1" role="button" aria-disabled="true">Logout</a>
-                  </div>
-                </div>
-                <?php
-            } else {
-                // Nếu chưa đăng nhập, hiển thị nút login
-                ?>
-                <div class="d-flex align-items-center">
-                  <a href="/user/login" class="btn btn-primary btn-lg " 
-                    tabindex="-1" role="button" aria-disabled="true">Login</a>
-                </div>
-                <?php
-            }
-            ?>
-          </a>
+
+      <?php
+      // Kiểm tra xem đã đăng nhập chưa            
+      if (UserController::isLoggedIn()) {
+        // Nếu đã đăng nhập, hiển thị nút logout và account
+      ?>
+        <div class="d-flex align-items-center">
+          <img src="/Public/images/account.png" id="user" class="rounded-circle me-3 account hide" height="30" alt="Black and White Portrait of a Man" />
+          <div class="d-flex align-items-center">
+            <a href="/user/logout" class="btn btn-primary btn-lg " tabindex="-1" role="button" aria-disabled="true">Logout</a>
+          </div>
         </div>
-      </div>
-      <!-- Right elements -->
+      <?php
+      } else {
+        // Nếu chưa đăng nhập, hiển thị nút login
+      ?>
+        <div class="d-flex align-items-center">
+          <a href="/user/login" class="btn btn-primary btn-xs " tabindex="-1" role="button" aria-disabled="true">Login</a>
+        </div>
+      <?php
+      }
+      ?>
+      </a>
+    </div>
+    </div>
+    <!-- Right elements -->
     </div>
     <!-- Container wrapper -->
   </nav>
   <!-- Navbar -->
-</body>
+</body> 
