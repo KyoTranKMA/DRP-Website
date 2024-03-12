4e41f93c29b897760cc3ef0b8543ef28c97da8f0
John Naeder <johnnaeder6537@gmail.com>
2024-03-13 04:37:54 +0700

    xoa cai breadline chuong mat

--- Web/App/Views/pages/homepage.php
+++ Web/App/Views/pages/homepage.php
@@ -1,24 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
+<? require($_SERVER['DOCUMENT_ROOT'] . "/Public/inc/header.php")?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang chủ</title>
-</head>
+<div class="container mb-3">
+    <div class="content d-flex justify-content-between align-items-center mt-3">
+        <div class="main-content">
+            <div class="card" style="width: 40rem;">
+                <a href="#">
+                    <img src="https://i.pinimg.com/564x/e7/72/7b/e7727b8a0ea5bbb4a596b04ec293ccd6.jpg" class="card-img-top" alt="Picture of meal" style="object-fit: cover; height:30rem">
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">Simple recipe for dinner</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Get recipe</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-content">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://i.pinimg.com/236x/20/dd/98/20dd98d752c6769f4b22ff3e0eeec867.jpg" class="card-img-bottom" alt="Picture of meal" style="object-fit: cover; height:12rem">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://i.pinimg.com/236x/57/78/f4/5778f4a3c1554a60a9673c14be613c70.jpg" class="card-img-bottom" alt="Picture of meal" style="object-fit: cover; height:12rem">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://i.pinimg.com/236x/3d/0d/19/3d0d1921f50187d823b90bc0a64bd6c6.jpg" class="card-img-bottom" alt="Picture of meal" style="object-fit: cover; height:12rem">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="checkbmi mb-3 p-3">
+        <iframe src="https://www.mdcalc.com/calc/29/body-mass-index-bmi-body-surface-area-bsa"  style="width:85vw; height:695px" frameborder="0.5px"></iframe>
+    </div> -->
+</div>
 
-<body>
-    <!--
-        /*
-            Sau khi đăng nhập thành công User sẽ vào trang này
-            Giao diện cho người dùng ở file này
-        */
-    -->
-
-    <h1>Hello user!</h1>
-    <h2>Click <a href="/logout">here</a> to logout</h2>
-
-
-</body>
-
-</html>
+<? require($_SERVER['DOCUMENT_ROOT'] . "/Public/inc/footer.php");?>
