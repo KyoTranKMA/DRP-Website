38be27ed60491d58efb5adf1fbb835ce1975aed4
maris-git <vtbtuyen2222004@gmail.com>
2024-03-13 14:34:33 +0700

    fix footer margin

--- Web/Public/inc/footer.php
+++ Web/Public/inc/footer.php
@@ -1,23 +1,23 @@
 <!-- Giới thiệu -->
 <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-0 px-4 px-xl-5" style="background-color: #e1ebfa;">
     <!-- Thông tin nhóm -->
-    <div class="text-black mb-3 mb-md-0">
-        <p style="font-weight: 700; font-size: 25px">PaPals</p>
-        <div class="mb-3 mb-md-0 me-3">
-            <p style="font-weight: 700">Easy Recipe for your delious meals</p>
+    <div class="text-black mb-md-0">
+        <p style="font-weight: 700; font-size: 25px;">PaPals</p>
+        <div class="mb-md-0 me-3">
+            <p style="font-weight: 700; margin: bottom 5px;">Easy Recipe for your delious meals</p>
         </div>
     </div>
     <!-- Thành viên -->
     <div class=" d-flex flex-column flex-md-row text-center text-md-start justify-content-between text-black">
-        <div class="mb-3 mb-md-0 me-3">
+        <div class="mb-md-0 me-3">
             <p style="font-weight: 700">Menu</p>
             <p>Breakfast</p>
         </div>
-        <div class="mb-3 mb-md-0 me-3">
+        <div class="mb-md-0 me-3">
             <p>Lunch</p>
             <p>Dinner</p>
         </div>
-        <div class="mb-3 mb-md-0">
+        <div class="mb-md-0">
             <p>Check BMI</p>
             <p>Check Ingredient</p>
         </div>
@@ -27,10 +27,15 @@
     <!-- Email nhóm -->
     <div>
         <p>Liên hệ</p>
-        <a href="#!" class="text-black mb-3 mb-md-0">
+        <a href="#!" class="text-black mb-md-0">
             <p>abc@gmail.com</p>
         </a>
     </div>
 </div>
 </bode>
 </html>
+<style>
+    p{
+        margin: 0.25rem;
+    }
+</style>
