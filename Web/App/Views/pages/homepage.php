fbb98afd840c871d05d304ece0545e548a4511c1
John Naeder <johnnaeder6537@gmail.com>
2024-03-18 03:28:44 +0700

    fix image not found error

--- Web/App/Views/pages/homepage.php
+++ Web/App/Views/pages/homepage.php
@@ -1,4 +1,4 @@
-<? require($_SERVER['DOCUMENT_ROOT'] . "/Public/inc/header.php")?>
+<? require($_SERVER['DOCUMENT_ROOT'] . "/Public/inc/header.php") ?>
 
 <div class="homepage">
     <div class="container mb-3">
@@ -6,8 +6,9 @@
             <? $mainRecipe = $data[0]; ?>
             <div class="main-content flex-fill" style="position: sticky; top: 1rem">
                 <div class="card" style="width: 45vw; height:83.5vh">
-                    <a href="#">
-                        <img src="/Public/uploads/recipes/<?echo $mainRecipe->getImgUrl() ?? "image_not_found.png" ; ?>" class="card-img-top" alt="Picture of meal" style="object-fit: cover; height:30rem">
+                    <a> 
+                     <img src="<?php echo ($mainRecipe->getImgUrl()) ? '/Public/uploads/recipes/' . $mainRecipe->getImgUrl() : '/Public/images/image_not_found.png' ?>" 
+                        alt="Recipe Image" class="card-img-top" alt="Picture of meal" style="object-fit: cover; height:30rem">
                     </a>
                     <div class="card-body">
                         <h5 class="card-title"><? echo $mainRecipe->getName() ?></h5>
@@ -18,22 +19,22 @@
             </div>
             <?php array_shift($data); ?>
             <div class="nav-content flex-fill ms-3">
-                <? foreach ($data as $recipe):?>
-                <a href="#" class="card mb-3" >
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="/Public/uploads/recipes/<?echo $recipe->getImgUrl()?? "no_image.jpg"?>" class="card-img-bottom" alt="Picture of meal" style="object-fit: cover; height:12rem">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title"><? echo htmlspecialchars($recipe->getName());?></h5>
-                                <p class="card-text"><? echo $recipe->getDescription()?></p>
-                                <p class="card-text"><small class="text-muted">Date: <? echo $recipe->getTimestamp() ?></small></p>
+                <? foreach ($data as $recipe) : ?>
+                    <a href="#" class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="<?php echo ($recipe->getImgUrl()) ? '/Public/uploads/recipes/' . $recipe->getImgUrl() : '/Public/images/image_not_found.png' ?>" class="card-img-bottom" alt="Picture of meal" style="object-fit: cover; height:12rem">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title"><? echo htmlspecialchars($recipe->getName()); ?></h5>
+                                    <p class="card-text"><? echo $recipe->getDescription() ?></p>
+                                    <p class="card-text"><small class="text-muted">Date: <? echo $recipe->getTimestamp() ?></small></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <?endforeach;?>
+                    </a>
+                <? endforeach; ?>
             </div>
         </div>
     </div>
