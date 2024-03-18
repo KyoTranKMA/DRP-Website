b749726c53a809d26a2e124c9c5d1a269371209b
maris-git <vtbtuyen2222004@gmail.com>
2024-03-18 15:43:57 +0700

    recipe pointer to recipe detail

--- Web/Public/js/ajax.js
+++ Web/Public/js/ajax.js
@@ -10,7 +10,7 @@ function viewRecipes(data) {
   $.each(recipes, function (index, recipe) {
     // Tạo thẻ div chứa thông tin của mỗi công thức
     var recipeDiv = $(
-        '<div class="card col-md-8" style="width: 22.5%; height: 25rem; margin: 1rem 1.25%">' +
+        '<div class="card col-md-8" style="width: 22.5%; height: 25rem; margin: 1rem 1.25%; cursor: pointer;">' +
           '<img src="' +
           (recipe.image_url ? "/Public/uploads/recipes/" + recipe.image_url : "/Public/images/" + "image_not_found.png") +
           '" class="card-img-top" alt="Picture of meal" style="object-fit: cover; height:12rem">' +
