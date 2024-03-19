b506cc0458153c73a478094d50a2bc236b6d08e7
YenLethanh129 <lethanhyen.work@gmail.com>
2024-03-19 23:14:59 +0700

    Re push

--- Web/App/Views/admin/index.php
+++ Web/App/Views/admin/index.php
@@ -14,23 +14,23 @@
           <div class="advert">
             <img src="/Public/images/account.png" class="img-thumbnail" alt="Ảnh user">
             <div class="link">    
-                <a href="/manager/user">Quản lý người dùng!</a>
+                <a href="/manager/user" class="btn btn-secondary" tabindex="-1" role="button">Quản lý người dùng!</a>
             </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="advert">
-            <img src="/Public/images/meal.jpg" class="img-thumbnail" style="height:415px" alt="ảnh recipe">
+            <img src="/Public/images/recipe.png" class="img-thumbnail" style="height:415px" alt="ảnh recipe">
             <div class="link">
-                <a href="/manager/recipe">Quản lý công thức!</a>
+                <a href="/manager/recipe" class="btn btn-secondary" tabindex="-1" role="button">Quản lý công thức!</a>
             </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="advert">
-            <img src="/Public/images/meal.jpg" class="img-thumbnail" style="height:415px" alt="ảnh ingredient">
+            <img src="/Public/images/ingredient.jpg" class="img-thumbnail" style="height:415px" alt="ảnh ingredient">
             <div class="link">
-                <a href="/manager/ingredient">Quản lý nguyên liệu!</a>
+                <a href="/manager/ingredient" class="btn btn-secondary" tabindex="-1" role="button">Quản lý nguyên liệu!</a>
             </div>
           </div>
         </div>
