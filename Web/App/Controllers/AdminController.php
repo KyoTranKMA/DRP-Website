69499bcf21143bdf812e84d7e0f2842a7396342f
John Naeder <johnnaeder6537@gmail.com>
2024-03-20 22:45:03 +0700

    Fix update recipe

--- Web/App/Controllers/AdminController.php
+++ Web/App/Controllers/AdminController.php
@@ -171,17 +171,15 @@ class AdminController extends BaseController{
             return parent::loadError('404');
         }
 
-        if($_GET['s_id'] != ''){ 
-            $ingredients = IngredientReadOperation::getSingleObjectById($_GET['s_id']);
-        } else if($_GET['s_name'] != ''){
-            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('name', $_GET['s_name']);
-        } else if ($_GET['s_category'] != ''){
-            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('s_category', $_GET['s_category']);
-        } else if ($_GET['s_measurement_desciption'] != ''){
-            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('s_measurement_desciption', $_GET['s_measurement_desciption']);
-        } else if ($_GET['s_name'] != ''){
-            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('s_name', $_GET['s_name']);
-        }
+        if($_GET['id'] != ''){ 
+            $ingredients = IngredientReadOperation::getSingleObjectById($_GET['id']);
+        } else if($_GET['name'] != ''){
+            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('name', $_GET['name']);
+        } else if ($_GET['category'] != ''){
+            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('category', $_GET['category']);
+        } else if ($_GET['measurement_desciption'] != ''){
+            $ingredients = IngredientReadOperation::getAllObjectsByFieldAndValue('measurement_description', $_GET['measurement_desciption']);
+        } 
 
         if(!$ingredients){
             $ingredients = IngredientReadOperation::getAllObjects();
